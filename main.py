339d1945d83a6909e47492fecc4efd7ed9e326ae
Suvansh7 <jindalsuvansh7@gmail.com>
2023-10-16 23:12:18 +0530

    dn

--- main.py
+++ main.py
@@ -5,17 +5,36 @@ from crop import sheet1,sheet2
 from locating import locate1
 
 imgc=0
-for i in range(2):
-    for j in range(1):
-        imgc=imgc+1
-        img=np.array(Image.open(f"C:\\Users\\Lenovo\\Desktop\\MiniProject\\set\\sheet{i+1}\\{j+1}.jpeg"))
-        # img = img.sum(2)/(255*3) # convert it intop greyscale
-        if(i==0):
-            cropped=sheet1(img)
-        elif(i==1):
-            cropped=sheet2(img)
-        else:
-            cropped=[]
+
+def set_of_3():
+    for i in range(2):
+        for j in range(1):
+            imgc=imgc+1
+            img=np.array(Image.open(f"C:\\Users\\Lenovo\\Desktop\\MiniProject\\set\\sheet{i+1}\\{j+1}.jpeg"))
+            # img = img.sum(2)/(255*3) # convert it intop greyscale
+            if(i==0):
+                cropped=sheet1(img)
+            elif(i==1):
+                cropped=sheet2(img)
+            else:
+                cropped=[]
 
         
-        locate1(cropped,imgc)
+            locate1(cropped,imgc)
+
+def single_sheet_set():
+    for i in range(2):
+        for j in range(1):
+            imgc=imgc+1
+            img=np.array(Image.open(f"C:\\Users\\Lenovo\\Desktop\\MiniProject\\set\\sheet{i+1}\\{j+1}.jpeg"))
+            # img = img.sum(2)/(255*3) # convert it intop greyscale
+            if(i==0):
+                cropped=sheet1(img)
+            elif(i==1):
+                cropped=sheet2(img)
+            else:
+                cropped=[]
+
+        
+            locate1(cropped,imgc)
+
